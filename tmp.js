e656f54952dec435c921fc2b54d00fbaebd3e096
Albert <albert_lyv@outlook.com>
2023-02-15 22:41:25 +0800

    create tmp.js

---
+++ tmp.js
@@ -0,0 +1,20 @@
+// ==UserScript==
+// @name         hello,this is a test
+// @namespace    https://codomos.site/      
+// @version      0.1
+// @description  try to take over the world!
+// @author       Albert
+// @match        *://*/*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=bilibili.com
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    console.log("hwllo,this is a test");
+
+
+    // Your code here...
+})
+();
